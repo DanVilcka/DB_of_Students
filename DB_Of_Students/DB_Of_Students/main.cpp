557ca9a4568021b25714a38b5c6ef64c41f9f9e6
Daniil <98778382+DanVilcka@users.noreply.github.com>
2023-05-14 14:34:52 +0300

    Test

---
+++ DB_Of_Students/DB_Of_Students/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DB_Of_Students
+//
+//  Created by Daniil Vilchinskiy on 14.05.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
